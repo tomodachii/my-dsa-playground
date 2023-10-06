9ea82cfbfefa1b4be62ca24b27df4e15b3790dd5
Hoang Do <tuanhoang2do2311@gmail.com>
2023-10-06 23:50:36 +0700

    Matrix operations: square matrix multiplication (#6)
    * matrix multiply
    
    * matrix operations: free allocated memory

--- matrix_operations/matrix_operations.h
+++ matrix_operations/matrix_operations.h
@@ -1,6 +1,8 @@
 #ifndef MATRIX_OPERATIONS_H
 #define MATRIX_OPERATIONS_H
 
+void square_matrix_add(int **A, int **B, int **C, int n);
 void square_matrix_multiply(int **A, int **B, int **C, int size);
+void square_matrix_multiply_recursive(int **A, int **B, int **C, int size);
 
 #endif /* MATRIX_OPERATIONS_H */
---
+++ matrix_operations/matrix_utils.h
@@ -0,0 +1,8 @@
+#ifndef MATRIX_UTILS_H
+#define MATRIX_UTILS_H
+
+// Function prototypes
+int** allocate_matrix(int rows, int cols);
+void free_matrix(int** matrix, int rows);
+
+#endif /* MATRIX_UTILS_H */
